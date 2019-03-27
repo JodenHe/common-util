a950e37f07104138c9add6fb449699498ce3d894
Joden_He <824923282@qq.com>
2019-03-27 09:36:27 +0800

    fix 改一下注释

--- src/main/java/com/github/jodenhe/utils/file/FileOperateUtil.java
+++ src/main/java/com/github/jodenhe/utils/file/FileOperateUtil.java
@@ -25,42 +25,40 @@ import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
-
 /**
- * 文件上传下载处理工具类
+ * file operation util
  * <p>
- * @author JodenHe
- * @create 2017年9月29日 下午4:30:54
+ * @author joden_he
+ * @since 2019-03-27
  */
 public class FileOperateUtil {
 
     private static Logger log = LoggerFactory.getLogger(FileOperateUtil.class);
 
-    /** 
-     * 下载 
-     *  
-     * @date 2012-5-5 下午12:25:39
-     * @param request 
-     * @param response 
-     * @param storeName 
-     * @param contentType 
-     * @param realName 
-     * @throws Exception 
-     */  
+    /**
+     * download file
+     * @param request
+     * @param response
+     * @param storeName
+     * @param contentType
+     * @param realName
+     * @throws Exception
+     */
     public static void download(HttpServletRequest request, HttpServletResponse response, String storeName, String contentType, String realName) throws Exception {
         String ctxPath = request.getSession().getServletContext().getRealPath("/");
         String downLoadPath = ctxPath + storeName;
         File file = new File(downLoadPath);
         file.renameTo(new File(ctxPath + realName));
         download(request, response, contentType, file);
-    }  
-    
+    }
+
 
     /**
-     * 下载 兼容文件名含有空格
+     * download file
      * @param request
      * @param response
      * @param contentType
+     * @param file
      * @throws Exception
      */
     public static void download(HttpServletRequest request, HttpServletResponse response, String contentType, File file) throws Exception {
@@ -80,6 +78,7 @@ public class FileOperateUtil {
             response.setContentType(contentType);
         } else {
             // 设置文件ContentType类型，这样设置，会自动判断下载文件类型
+            // set contentType to "multipart/form-data", will auto become file type
             response.setContentType("multipart/form-data");
         }
 
@@ -104,7 +103,11 @@ public class FileOperateUtil {
     }
 
     /**
-     * url 文件代理中转
+     * url file proxy download
+     * <p>
+     *     For example, http://xxx/aaa is an Intranet url and we need to download file by Extranet.
+     *     so we can use this method to proxy the Intranet file
+     * </p>
      * @param response
      * @param address
      * @param contentType
@@ -128,6 +131,7 @@ public class FileOperateUtil {
                 response.setContentType(contentType);
             } else {
                 // 设置文件ContentType类型，这样设置，会自动判断下载文件类型
+                // set contentType to "multipart/form-data", will auto become file type
                 response.setContentType("multipart/form-data");
             }
             if (fileName != null) {
@@ -138,7 +142,7 @@ public class FileOperateUtil {
             // 创建一个Buffer字符串
             byte[] buffer = new byte[1024];
             // 每次读取的字符串长度，如果为-1，代表全部读取完毕
-            int len = 0;
+            int len;
             // 使用一个输入流从buffer里把数据读取出来
             while((len = inputStream.read(buffer)) != -1 ){
                 // 用输出流往buffer里写入数据，中间参数代表从哪个位置开始读，len 代表读取的长度
